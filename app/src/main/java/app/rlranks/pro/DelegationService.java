4cb7ae85510ee37f61534e7388c3eebafe0c662d
WhalesLoveSmash <dannyrhalsteadbackup@gmail.com>
2025-09-03 09:54:31 -0500

    minor build

---
+++ app/src/main/java/app/rlranks/pro/DelegationService.java
@@ -0,0 +1,14 @@
+package app.rlranks.pro;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
